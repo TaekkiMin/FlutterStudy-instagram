8145f6fd3fc523a84d99481ea2ae4369969d8722
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-01-27 10:58:12 +0900

    feat : 계정 페이지 구현
    floatingactionbutton 함수를 통해 플러스 아이콘을 만들어 이미지 하단우측으로 배치

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -19,6 +19,120 @@ class AccountPage extends StatelessWidget {
           ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://img.extmovie.com/files/attach/images/148/873/786/091/c8d055c9df5123aa483e3324d81a54fa.jpg'),
+                      ),
+                    ),
+
+                    // 버튼 크기를 이미지 크기와 동일하게 만들어야만 alignment가 적용이 됨
+                    Container(
+                      width: 80,
+                      height: 80,
+                      alignment: Alignment.bottomRight,
+                      child: SizedBox(
+                        width: 25,
+                        height: 25,
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            // 버튼이 눌렸을 때 실행될 코드
+                          },
+                          backgroundColor:
+                              Colors.transparent, // 버튼의 배경 색상을 투명으로 설정
+                          elevation: 0,
+                          child: Container(
+                            width: 56.0, // 버튼의 너비
+                            height: 56.0, // 버튼의 높이
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.blue, // 파란색으로 설정
+                            ),
+                            child: const Center(
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white, // 아이콘의 색상을 흰색으로 설정
+                              ),
+                            ),
+                          ), // 그림자를 제거
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'im_winter',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '게시물',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  '3',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '팔로워',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  '188',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+            const Column(
+              children: [
+                Text(
+                  '팔로잉',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Text(
+                  '297',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
